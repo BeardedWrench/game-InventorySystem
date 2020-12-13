3ef2ba276e0a4d120d63b2811bdcdd30b036ba83
Terry Brown <tabrownj@gmail.com>
2020-12-12 20:49:46 -0500

    Updated ReadMe, added template comment for adding a player to inventory.js

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# game-InventorySystem
+# Basic Inventory system for a game.
+
+This is just a simple inventory system to show off the benefits of objects/arrays in Javascript.
+
+## Adding Items & Players
+
+### Adding an Item to the 'Database'
+```
+new Item( "Name", "Display Type", weight, damage, armor, stackable?, icon-path, model-path, type )
+```
+
+### Adding an Player
+```
+new Player( "name", "Display Name", health, money, group )
+```
--- scripts/inventory.js
+++ scripts/inventory.js
@@ -2,6 +2,9 @@ import { Player } from '/scripts/player.js';
 import { Item, weapons, armor } from '/scripts/item.js';
 
 const playersList = [];
+//Template for Player: new Player( "name", "Display Name", health, money, group )
+
+
 const terry = new Player( 'Terry', 'BeardedWrench', 100, 687, playersList );
 const bob = new Player( 'Robert', 'xBoBx2020', 100, 200, playersList );
 const jane = new Player( 'Jane', 'JaneDoe1993', 100, 122, playersList );
