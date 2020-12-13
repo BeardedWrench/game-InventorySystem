e2c23bf75e571ec982fe1c734d204abb4e48cb50
Terry Brown <tabrownj@gmail.com>
2020-12-12 22:17:55 -0500

    Added some ui adjustments

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -17,13 +17,16 @@ console.log( "Player-Terry: ", terry );
 
 
 //cheap code to just display some icons from the "database"
-let markup = '<div class="item"><img src="%icon%"/></div>'
+let markup = '<div class="item"><img src="%icon%"/><h3>%name%</h3></div>'
 
 for( let i = 0; i < weapon.length; i++){
     let weaponList = markup.replace('%icon%', weapon[i].icon);
+    weaponList = weaponList.replace('%name%', weapon[i].name);
+
     document.querySelector(".item-list").insertAdjacentHTML('beforeend', weaponList);
 }
 for( let i = 0; i < armor.length; i++){
     let armorList = markup.replace('%icon%', armor[i].icon);
+    armorList = armorList.replace('%name%', armor[i].name);
     document.querySelector(".item-list").insertAdjacentHTML('beforeend', armorList);
 }
