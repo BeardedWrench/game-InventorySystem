82bf9a5476701a058229b0e2588dc7cb161e7685
Terry Brown <tabrownj@gmail.com>
2020-12-12 22:31:22 -0500

    added some UI for player inventory READ ONLY

--- scripts/inventory.js
+++ scripts/inventory.js
@@ -15,18 +15,25 @@ terry.inventory.addItem(weapon[1], 1);
 console.log( "Players List: ", playersList );
 console.log( "Player-Terry: ", terry );
 
+console.log(terry.inventory.items);
 
-//cheap code to just display some icons from the "database"
-let markup = '<div class="item"><img src="%icon%"/><h3>%name%</h3></div>'
 
+//cheap/ really BAD code to just display some icons from the "database"
+let dbMarkup = '<div class="item"><img src="%icon%"/><h3>%name%</h3></div>'
 for( let i = 0; i < weapon.length; i++){
-    let weaponList = markup.replace('%icon%', weapon[i].icon);
+    let weaponList = dbMarkup.replace('%icon%', weapon[i].icon);
     weaponList = weaponList.replace('%name%', weapon[i].name);
 
     document.querySelector(".item-list").insertAdjacentHTML('beforeend', weaponList);
 }
 for( let i = 0; i < armor.length; i++){
-    let armorList = markup.replace('%icon%', armor[i].icon);
+    let armorList = dbMarkup.replace('%icon%', armor[i].icon);
     armorList = armorList.replace('%name%', armor[i].name);
     document.querySelector(".item-list").insertAdjacentHTML('beforeend', armorList);
+}
+
+for( let i = 0; i < terry.inventory.items.length; i++){
+    let plyList = dbMarkup.replace('%icon%', terry.inventory.items[i].icon);
+    plyList = plyList.replace('%name%', terry.inventory.items[i].name);
+    document.querySelector(".player-items").insertAdjacentHTML('beforeend', plyList);
 }
